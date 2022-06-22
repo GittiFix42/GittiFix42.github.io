36f189c45bf3efab1dc546c7deaff9d56ec47729
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-06-22 19:21:26 +0200

    moved recipies to Kochbuch

--- makefile.sh
+++ makefile.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#echo "Hello world!"
 for file in ./*.adoc ; do 
     asciidoctor-pdf $file -a pdf-theme=my-theme.yml
-	#echo "$file als PDF generiert"
-	#fname=$(basename "$file")
     for pdffile in ./${file%adoc}pdf ; do 
 	    echo "Neues PDF: $pdffile"
+        mv $pdffile Kochbuch/$pdffile
+        cd Kochbuch
         git add $pdffile
+        cd ..
 	done
-    #fdir=$(dirname "$file")
-	#echo "und steht im Verzeichnis: $fdir"
 done
-#date
 jetzt=$(date)
 git commit -m "Generate all PDF at $jetzt"
