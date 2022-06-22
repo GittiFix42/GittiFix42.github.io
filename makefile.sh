99d1d22093e486b3b340817ec0387cb169c5ee97
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-06-22 15:26:16 +0200

    defailtContxt

--- makefile.sh
+++ makefile.sh
@@ -2,9 +2,14 @@
 echo "Hello world!"
 for file in ./*.adoc ; do 
     asciidoctor-pdf $file -a pdf-theme=my-theme.yml
-	#echo "Diese Datei: $file"
-	#fname=$(basename "$file")
-	#echo "hat den Namen: $fname"
-	#fdir=$(dirname "$file")
+	#echo "$file als PDF generiert"
+	fname=$(basename "$file")
+    for pdffile in ./$fname.pdf ; do 
+	    echo "Neues PDF: $pdffile"
+        git add $pdffile
+	done
+    #fdir=$(dirname "$file")
 	#echo "und steht im Verzeichnis: $fdir"
 done
+jetzt=date
+git commit -m "Generate all PDF at $jetzt"
