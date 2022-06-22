559ef4273c32c5da62abaf4595da18e284df74a8
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-06-22 15:02:29 +0200

    Alles neu generiert

--- makefile.sh
+++ makefile.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "Hello world!"
+for file in ./*.adoc ; do 
+    asciidoctor-pdf $file -a pdf-theme=my-theme.yml
+	#echo "Diese Datei: $file"
+	#fname=$(basename "$file")
+	#echo "hat den Namen: $fname"
+	#fdir=$(dirname "$file")
+	#echo "und steht im Verzeichnis: $fdir"
+done
