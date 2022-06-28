19665724793cf4aed76e1e534a47f23c62c0e0a2
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-06-28 15:34:04 +0200

    Alles ins Makefile?

--- makefile.sh
+++ makefile.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 mkdir HtmlKochbuch
-#for file in ./*.adoc ; do 
+for file in ./*.adoc ; do 
 #    #asciidoctor-pdf $file -a pdf-theme=my-theme.yml
 #    asciidoctor $file 
-#    for htmlfile in ./${file%adoc}html ; do 
-#	    echo "Neues html: $htmlfile"
-#        mv $htmlfile HtmlKochbuch/$htmlfile
-#	done
-#done
+    docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf $file
+    for htmlfile in ./${file%adoc}html ; do 
+	    echo "Neues html: $htmlfile"
+        mv $htmlfile HtmlKochbuch/$htmlfile
+	done
+done
 for jpegfile in ./*.jpeg ; do 
     echo "jpeg: $jpegfile"
     mv $jpegfile HtmlKochbuch/$jpegfile
