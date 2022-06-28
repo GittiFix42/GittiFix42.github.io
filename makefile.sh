bceeca65b8eec035412d1d63aa0eb369735eb4d2
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-06-28 15:05:27 +0200

    Workflow ueberarbeitet.

--- makefile.sh
+++ makefile.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 mkdir HtmlKochbuch
-for file in ./*.adoc ; do 
-    #asciidoctor-pdf $file -a pdf-theme=my-theme.yml
-    asciidoctor $file 
-    for htmlfile in ./${file%adoc}html ; do 
-	    echo "Neues html: $htmlfile"
-        mv $htmlfile HtmlKochbuch/$htmlfile
-	done
-done
+#for file in ./*.adoc ; do 
+#    #asciidoctor-pdf $file -a pdf-theme=my-theme.yml
+#    asciidoctor $file 
+#    for htmlfile in ./${file%adoc}html ; do 
+#	    echo "Neues html: $htmlfile"
+#        mv $htmlfile HtmlKochbuch/$htmlfile
+#	done
+#done
 for jpegfile in ./*.jpeg ; do 
     echo "jpeg: $jpegfile"
     mv $jpegfile HtmlKochbuch/$jpegfile
+done
+for htmlfile in ./*.html ; do 
+    echo "html: $htmlfile"
+    mv $htmlfile HtmlKochbuch/$htmlfile
 done
