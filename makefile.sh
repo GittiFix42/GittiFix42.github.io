40272420837a641800319425edffcaa31bfe8c4b
GittiFix42 <107473145+GittiFix42@users.noreply.github.com>
2022-07-04 14:27:13 +0200

    Korrektur und Formatierung der Urheberangabe.

--- makefile.sh
+++ makefile.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 mkdir HtmlKochbuch
 for file in ./*.adoc ; do 
-#    #asciidoctor-pdf $file -a pdf-theme=my-theme.yml
     asciidoctor $file -a stylesheet=Stylesheet.css
-    #docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf $file
-    #for htmlfile in ./${file%adoc}html ; do 
-	#    docker --rm -v $(pwd):/documents/ run pwd
-    #    docker --rm -v $(pwd):/documents/ run ls -altr
-    #    echo "Neues html: $htmlfile"
-    #    docker --rm -v $(pwd):/documents/ run mv $htmlfile HtmlKochbuch/$htmlfile
-	#done
 done
 for jpegfile in ./*.jpeg ; do 
     echo "jpeg: $jpegfile"
